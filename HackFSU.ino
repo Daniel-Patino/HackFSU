a77c2da98ff2e04bb0657d100dc2bccb455317f0
DESKTOP-4CDN43V\Daniel <dpati007@fiu.edu>
2017-02-18 01:27:35 -0500

    Added Line

--- HackFSU.ino
+++ HackFSU.ino
@@ -6,5 +6,6 @@ void setup()
 void loop()
 {
   Serial.write(45); // send a byte with the value 45
+  
   int bytesSent = Serial.write(“hello”); //send the string “hello” and return the length of the string.
 }
